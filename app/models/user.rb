70ac4c7d10aa0ea5943d492574e1a8d41a860135
ttowatt <tanakatowa852@gmail.com>
2025-10-07 15:00:17 +0000

    Add search function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[username]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
