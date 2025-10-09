54b2f2249b829a4d2f808955951843a3deae5c84
ttowatt <tanakatowa852@gmail.com>
2025-10-09 14:14:24 +0000

    Progress basic-functions

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,4 @@
 class History < ApplicationRecord
+  belongs_to :Category
+  belongs_to :expense
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_one_attached :image
+
+         has_many :category
+         has_many :active_relationships, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+         has_many :following, through: :relationships, source: :followed
+         has_many :reverse_relationships, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+         has_many :followers, through: :reverse_relationships, source: :follower
+
   def self.ransackable_attributes(auth_object = nil)
     %w[username]
   end
