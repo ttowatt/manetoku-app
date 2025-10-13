e9db7a181c8c3d9445e262cd8c538bfe411e8886
ttowatt <tanakatowa852@gmail.com>
2025-10-13 14:24:59 +0000

    progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_11_032054) do
+ActiveRecord::Schema.define(version: 2025_10_13_085642) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2025_10_11_032054) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
-    t.integer "followed__id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
