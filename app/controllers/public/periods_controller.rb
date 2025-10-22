19bff3e9d129a38018c3e27b4faf3338d96c6390
ttowatt <tanakatowa852@gmail.com>
2025-10-22 09:55:13 +0000

    Progress

--- app/controllers/public/periods_controller.rb
+++ app/controllers/public/periods_controller.rb
@@ -1,18 +1,39 @@
 class Public::PeriodsController < ApplicationController
   def index
+    #新しい期間（最新のstart_date）から順に並べる
       @periods = current_user.periods.includes(:categories).order(start_date: :desc)
   end
 
   def show
+    @period = current_user.periods.find(params[:id])
+
+    # この期間に紐づくカテゴリを事前に支出も含めて取得
+    @categories = @period.categories.includes(:expenses)
   end
 
   def create
-    @period = Period.new(period_params)
-    @period.user_id = current_user.id
+    @period = current_user.periods.new(period_params)
+  
     if @period.save
-      redirect_to root_path, notice: "登録が完了しました"
+      # 直前の期間を取得(最新の期間を1件のみ表示)
+      previous_period = current_user.periods.order(created_at: :desc).second
+  
+      # 前の期間が存在すれば、そのカテゴリを引き継ぐ
+      if previous_period.present?
+        previous_period.categories.each do |category|
+          @period.categories.create!(
+            user: current_user,
+            category_name: category.category_name,
+            budget: category.budget
+          )
+        end
+      end
+  
+      flash[:notice] = "期間が作成されました（前のカテゴリを引き継ぎました）"
+      redirect_to root_path
     else
-      redirect_to root_path, alert: "登録に失敗しました"
+      flash.now[:alert] = "期間の作成に失敗しました"
+      render "homes/top"
     end
   end
 
@@ -30,8 +51,10 @@ class Public::PeriodsController < ApplicationController
   end
 
   def destroy
+    @period = current_user.periods.find(params[:id])
     @period.destroy
-    redirect_to public_histories_path, notice: "削除しました。"
+    flash[:notice] = "期間を削除しました"
+    redirect_to root_path
   end
 
   private
