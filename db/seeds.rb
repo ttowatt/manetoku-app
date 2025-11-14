f986852bc2867fa88070df82bd14a02e20b8173e
ttowatt <tanakatowa852@gmail.com>
2025-11-14 13:35:59 +0000

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,53 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#1stデプロイのテスト内容
 puts "seedの実行を開始"
 
-Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
+Admin.find_or_create_by!(email: ENV.fetch("ADMIN_EMAIL")) do |admin|
   admin.password = ENV.fetch("ADMIN_PASSWORD")
   admin.password_confirmation = ENV.fetch("ADMIN_PASSWORD")
 end
 
-<<<<<<< HEAD
 ludo = User.find_or_create_by!(email: "ludo@example.com") do |user|
+
+manetoku = User.find_or_create_by!(email: ENV.fetch("PUBLIC_EMAIL")) do |user|
+  user.last_name = "田中"
+  user.first_name = "和"
+  user.last_name_kana = "タナカ"
+  user.first_name_kana = "カズ"
+  user.phone_number = "09087654321"
+  user.username = "manetoku"
+  user.introduction = "hello manetoku"
+  user.password = ENV.fetch("PUBLIC_PASSWORD")
+  user.profile_image.attach(
+    io: File.open("#{Rails.root}/db/fixtures/sample-user3.png"),
+    filename: "sample-user3.png"
+  )
+end
+
+period = Period.find_or_create_by!(
+  start_date: Date.new(2025,10,26),
+  end_date: Date.new(2025,10,27),
+  user_id: manetoku.id
+)
+
+category = Category.find_or_create_by!(
+  category_name: "通信費",
+  budget: 18000,
+  user_id: manetoku.id,
+  period_id: period.id
+)
+
+Expense.find_or_create_by!(
+  amount: 900,
+  expense_date: Date.new(2025,10,27),
+  user_id: manetoku.id,
+  period_id: period.id,
+  category_id: category.id
+)
+
+# フォロー関係
+other_user = User.find_or_create_by!(email: ENV.fetch("USER_EMAIL")) do |user|
   user.last_name = "斎藤"
   user.first_name = "久夫"
   user.last_name_kana = "サイトウ"
@@ -23,83 +60,40 @@ ludo = User.find_or_create_by!(email: "ludo@example.com") do |user|
   user.phone_number = "09048489696"
   user.username = "ludo"
   user.introduction = "hello japan"
-  user.password = "password456"
-=======
-lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
-  user.last_name = "佐藤"
-  user.first_name = "道子"
-  user.last_name_kana = "サトウ"
-  user.first_name_kana = "ミチコ"
-  user.phone_number = "09098765432"
-  user.username = "Lucas"
-  user.introduction = "hello"
-  user.password = "password1"
->>>>>>> e621dfd (Fix merge conflict)
   user.profile_image.attach(
     io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"),
     filename: "sample-user3.jpg"
   )
 end
 
-<<<<<<< HEAD
 post = Post.find_or_create_by!(title: "革命的", user_id: ludo.id) do |p|
+  user.password = ENV.fetch("USER_PASSWORD")
+end
+
+# other_userがmanetokuをフォロー
+Follow.find_or_create_by!(follower_id: other_user.id, followed_id: manetoku.id)
+
+post = Post.find_or_create_by!(title: "革命的", user_id: manetoku.id) do |p|
   p.category = "交際費"
   p.body = "知らないと損！"
   p.post_image.attach(
-    io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"),
-    filename: "sample-post3.jpg"
+    io: File.open("#{Rails.root}/db/fixtures/sample-post3.png"),
+    filename: "sample-post3.png"
   )
 end
 
-#2stデプロイのテスト内容
 Comment.find_or_create_by!(
   body: "非常にためになった",
   post_id: post.id,
-  user_id: ludo.id
+  user_id: other_user.id
 )
-
-period = Period.find_or_create_by!(
-  start_date: Date.new(2025,10,26),
-  end_date: Date.new(2025,10,27),
-  user_id: ludo.id
-)
-
-category = Category.find_or_create_by!(
-  category_name: "通信費",
-  budget: 18000,
-  user_id: ludo.id,
-  period_id: period.id
-)
-
-Expense.find_or_create_by!(
-  amount: 900,
-  expense_date: Date.new(2025,10,27),
-  user_id: ludo.id,
-  period_id: period.id,
-  category_id: category.id
-)
-
-# フォロー関係
-other_user = User.find_or_create_by!(email: "public@example.com") do |user|
-  user.last_name = "田中"
-  user.first_name = "花子"
-  user.last_name_kana = "タナカ"
-  user.first_name_kana = "ハナコ"
-  user.phone_number = "09012345678"
-  user.username = "tanaka"
-  user.introduction = "hello"
-  user.password = "public1"
-end
-
-# ludoがother_userをフォロー
-Follow.find_or_create_by!(follower_id: ludo.id, followed_id: other_user.id)
 
 # 投稿へのいいね
 PostLike.find_or_create_by!(user_id: other_user.id, post_id: post.id)
 
 # コメントへのいいね
 comment = Comment.find_by(post_id: post.id)
-CommentLike.find_or_create_by!(user_id: other_user.id, comment_id: comment.id)
+CommentLike.find_or_create_by!(user_id: manetoku.id, comment_id: comment.id)
 
 # レビュー（評価）
 Review.find_or_create_by!(
@@ -111,13 +105,12 @@ Review.find_or_create_by!(
 
 # 通知(フォローしているユーザーが投稿)
 Notification.find_or_create_by!(
-  visitor_id: other_user.id,
-  visited_id: ludo.id,
-  post_id: post.id,
-  is_read: true
+  user_id: other_user.id,
+  notifiable_type: "Post",
+  notifiable_id: post.id,
+  read: true
 )
 
-=======
 Post.find_or_create_by!(title: "節約術") do |post|
   post.body = "自慢の節約術"
   post.post_image.attach(
@@ -127,6 +120,5 @@ Post.find_or_create_by!(title: "節約術") do |post|
   post.user = lucas
 end
 
->>>>>>> e621dfd (Fix merge conflict)
 puts "seedの実行が完了しました"
 
