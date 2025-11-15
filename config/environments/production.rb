016dddb944470379cf2323d2556e0a6d077e295c
ttowatt <tanakatowa852@gmail.com>
2025-11-15 06:04:24 +0000

    Update storage.yml/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.asset_host = ENV.fetch("AWS_S3_HOST_NAME")# CloudFrontディストリビューションのドメインを追加
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
